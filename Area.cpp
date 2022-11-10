62226ba1410dc2a3ae0b9ebb399b0d3b08c65f0e
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-11-10 09:02:16 -0800

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int base, height;
+    float area;
+    cout << "Enter the numbers\n";
+    cin >> base;
+    cin >> height;
+    area = base * height * 0.5;
+    cout << "are is " << area << "\n";
+    return 0;
+}
